3cc0c026f05413dd5ec3d94504ce562a4089cc64
Marcusjenks <me@marcusjenks.com>
2017-12-08 15:33:30 -0500

    Implement Basic XML support
    Basic XML support for Enemy class. Random enemy retrieved from XML file.

--- src/battle/Battle.java
+++ src/battle/Battle.java
@@ -3,14 +3,15 @@ package battle;
 public class Battle {
 
 	public static void main(String[] args) {
-		Enemy[] enemy;
-		enemy = new Enemy[1];
-		enemy[0] = new Enemy("Bat", 5, 1);
-		
+		Enemy enemy;
+		enemy = new Enemy();
+	
 		Prompt playerName = new Prompt();
 		Player player = new Player(playerName.getString("Welcome to battletext! What is your name?"), 200, 20);
 		System.out.println("Hello " + player.getName() + ". You have " + player.getHealth() + " HP and " + player.getStrength() + " Strength.");
-		System.out.println("Enemy: " + enemy[0].getName() + " Health: " + enemy[0].getHealth() + " Strength: " + enemy[0].getStrength());
-	}
+		System.out.println("Enemy: " + enemy.getName() + " | Health: " + enemy.getHealth() + " | Strength: " + enemy.getStrength());
+		
 
 }
+}
+
--- src/battle/Enemy.java
+++ src/battle/Enemy.java
@@ -1,17 +1,51 @@
 package battle;
 
+import java.util.Random;
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class Enemy {
 	
-	public Enemy(String name, int health, int strength) {
-		this.name = name;
-		this.health = health;
-		this.strength = strength;
+	public Enemy() {
+		
+		try {
+			
+			File fXmlFile = new File("enemy.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+						
+			NodeList nList = doc.getElementsByTagName("Enemy");
+			
+				Random random = new Random();
+
+				Node nNode = nList.item(random.nextInt(nList.getLength()));
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					
+					name = eElement.getElementsByTagName("Name").item(0).getTextContent();
+					health = Integer.parseInt(eElement.getElementsByTagName("Health").item(0).getTextContent());
+					strength = Integer.parseInt(eElement.getElementsByTagName("Strength").item(0).getTextContent());					
+					
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
 	
 	private String name;
 	private int health;
 	private int strength;
+
 	
 	public String getName() {
 		return name;
@@ -24,6 +58,5 @@ public class Enemy {
 	public int getStrength() {
 		return strength;
 	}
-	
-
 }
+
