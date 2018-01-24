264b63e3af2ab10c623b12c484fd16351a41c1be
marcusjenks <me@marcusjenks.com>
2018-01-23 21:36:27 -0500

    Refactor running and fighting loop
    Refactored running and fighting loops to remove breaks and improve
    readabliity.

--- src/battle/Battle.java
+++ src/battle/Battle.java
@@ -10,51 +10,50 @@ public class Battle {
 		String choice;
 		String answer;
 		Enemy enemy;
-		
+
 		boolean running = true;
-		
+
 		while (running) {
 			enemy = new Enemy();
-			
+
 			System.out.println("\nYou have encountered: " + enemy.getName() + "! \n");
-									
-			while(enemy.getHealth() > 0 && player.getHealth() >0) {
-				
+
+			boolean fighting = true;
+			while (fighting) {
 				enemy.getInfo();
 
 				player.getInfo();
 
 				choice = playerAction.getAttack("\nWhat will you do? \n(1) Attack \n(2) Run away");
-				
-				if (choice.equals("1")) {				
+
+				if (choice.equals("1")) {
 					enemy.takeDamage(player.getStrength());
-			
-					if(enemy.getHealth() <= 0) {
+
+					if (enemy.getHealth() <= 0) {
 						enemy.dead();
-						break;
+						fighting = false;
 					}
-			
+
 					player.takeDamage(enemy.getStrength());
-				}
-				else {
-					break;
+
+					if (player.getHealth() <= 0) {
+						fighting = false;
+					}
+				} else {
+					fighting = false;
 				}
 			}
 
-		
 			if (player.getHealth() <= 0) {
-				System.out.println("The battle for glory has ended. Perhaps you will have better luck in the next life...");
+				System.out.println(
+						"The battle for glory has ended. Perhaps you will have better luck in the next life...");
 				running = false;
-				break;
-			}
-			
-			answer = playAgain.getAnswer("Will you fight again for GLORY? y/n");
-			if (answer.equals("n")){
-				running = false;
+			} else {
+				answer = playAgain.getAnswer("Will you fight again for GLORY? y/n");
+				if (answer.equals("n")) {
+					running = false;
+				}
 			}
 		}
-		
-
+	}
 }
-}
-
