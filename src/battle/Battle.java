9b42be249dca90a73e72bcd35e2abc56fe779a1a
marcusjenks <me@marcusjenks.com>
2018-01-23 22:50:46 -0500

    Refactor prompt class
    Refactored prompt class to simplify its responsibilities.

--- src/battle/Battle.java
+++ src/battle/Battle.java
@@ -24,23 +24,35 @@ public class Battle {
 
 				player.getInfo();
 
-				choice = playerAction.getAttack("\nWhat will you do? \n(1) Attack \n(2) Run away");
+				choice = playerAction.getString("\nWhat will you do? \n(1) Attack \n(2) Run away");
+				boolean response = true;
+				while (response) {
+					if (choice.equals("1")) {
+						System.out.println("\nYou attack!\n");
+						enemy.takeDamage(player.getStrength());
 
-				if (choice.equals("1")) {
-					enemy.takeDamage(player.getStrength());
+						if (enemy.getHealth() <= 0) {
+							enemy.dead();
+							fighting = false;
+							response = false;
+						}
 
-					if (enemy.getHealth() <= 0) {
-						enemy.dead();
+						player.takeDamage(enemy.getStrength());
+
+						if (player.getHealth() <= 0) {
+							fighting = false;
+							response = false;
+						}
+
+						response = false;
+
+					} else if (choice.equals("2")) {
+						System.out.println("You run away! Coward...");
 						fighting = false;
+						response = false;
+					} else {
+						choice = playerAction.getString("Please use correct input!");
 					}
-
-					player.takeDamage(enemy.getStrength());
-
-					if (player.getHealth() <= 0) {
-						fighting = false;
-					}
-				} else {
-					fighting = false;
 				}
 			}
 
@@ -49,9 +61,19 @@ public class Battle {
 						"The battle for glory has ended. Perhaps you will have better luck in the next life...");
 				running = false;
 			} else {
-				answer = playAgain.getAnswer("Will you fight again for GLORY? y/n");
-				if (answer.equals("n")) {
-					running = false;
+				answer = playAgain.getString("Will you fight again for GLORY? y/n");
+				boolean ans = true;
+				while (ans) {
+					if (answer.equals("n")) {
+						System.out.println("Pathetic");
+						running = false;
+						ans = false;
+					} else if (answer.equals("y")) {
+						System.out.println("Fight on for glory!");
+						ans = false;
+					} else {
+						answer = playAgain.getString("Please use correct input");
+					}
 				}
 			}
 		}
--- src/battle/Prompt.java
+++ src/battle/Prompt.java
@@ -1,4 +1,5 @@
 package battle;
+
 import java.util.Scanner;
 
 public class Prompt {
@@ -7,41 +8,9 @@ public class Prompt {
 	public Prompt() {
 		scanner = new Scanner(System.in);
 	}
-	
+
 	public String getString(String prompt) {
 		System.out.println(prompt);
 		return scanner.nextLine();
-	}
-	
-	public String getAttack(String prompt) {
-		System.out.println(prompt);
-		String ans = scanner.nextLine();
-		if (ans.equals("1")){
-			System.out.println("\nYou attack!\n");
-		}
-		else if (ans.equals("2")) {
-			System.out.println("\nYou run away! Coward...\n");
-		}
-		else {
-			getAttack("Please use correct input!");
-		}
-		return ans;
-	}
-	
-	public String getAnswer(String prompt) {
-		System.out.println(prompt);
-		String ans = scanner.nextLine();
-		if (ans.equals("y")){
-			System.out.println("\nFIGHT ON FOR GLORY.\n");
-
-		}
-		else if (ans.equals("n")){
-			System.out.println("\nPathetic.\n");
-
-		}
-		else {
-			getAnswer("Please use correct input!");
-		}
-		return ans;
 	}
 }
